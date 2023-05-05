03d3ae66603e9b680b2c1c9e4fc8f300de1d873b
javohir <javokhir544@gmail.com>
2023-05-05 15:46:27 +0500

    commit 2

--- src/main/java/uz/market/uzum/services/user/UserService.java
+++ src/main/java/uz/market/uzum/services/user/UserService.java
@@ -1,5 +1,7 @@
 package uz.market.uzum.services.user;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +13,7 @@ import uz.market.uzum.repositories.user.UserRepository;
 import java.util.Collection;
 
 @Service
+@CacheConfig(cacheNames = "users")
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
@@ -19,16 +22,20 @@ public class UserService implements UserDetailsService {
     }
 
 
+    @Cacheable(key = "#username")
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByPhoneNumber(username);
     }
 
+    @Cacheable(key = "#userId")
     public User getUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found with id " + userId));
     }
 
+    @Cacheable(key = "#root.methodName")
     public Collection<User> findAll() {
         return userRepository.findAll();
     }
+
 }
