dd6dc56882c2b89b0855d8ff9bfc5ebfcb43504f
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-05-07 09:21:54 +0500

    category

--- target/generated-sources/annotations/uz/market/uzum/mappers/roles/UserRoleMapperImpl.java
+++ target/generated-sources/annotations/uz/market/uzum/mappers/roles/UserRoleMapperImpl.java
@@ -6,7 +6,7 @@ import uz.market.uzum.dtos.roles.UserRoleCreateDTO;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-07T07:43:20+0500",
+    date = "2023-05-07T09:15:42+0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Private Build)"
 )
 public class UserRoleMapperImpl implements UserRoleMapper {
