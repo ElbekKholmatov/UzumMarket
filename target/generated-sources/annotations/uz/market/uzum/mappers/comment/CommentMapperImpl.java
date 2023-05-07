bae8b536339e55617b67f3c34a813df6934c99f7
Tulaev Muhammad <tulaevmuhammadman@gmail.com>
2023-05-07 10:37:34 +0500

    Tamom(Included Test)

---
+++ target/generated-sources/annotations/uz/market/uzum/mappers/comment/CommentMapperImpl.java
@@ -0,0 +1,63 @@
+package uz.market.uzum.mappers.comment;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.market.uzum.domains.product.Comment;
+import uz.market.uzum.dtos.comment.CommentCreateDto;
+import uz.market.uzum.dtos.comment.CommentUpdateDTO;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-07T10:30:06+0500",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+
+    @Override
+    public CommentCreateDto toDto(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentCreateDto.CommentCreateDtoBuilder commentCreateDto = CommentCreateDto.builder();
+
+        commentCreateDto.text( comment.getText() );
+        commentCreateDto.rate( comment.getRate() );
+        commentCreateDto.productId( comment.getProductId() );
+
+        return commentCreateDto.build();
+    }
+
+    @Override
+    public Comment toEntity(CommentCreateDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Comment.CommentBuilder comment = Comment.commentBuilder();
+
+        comment.text( dto.getText() );
+        comment.rate( dto.getRate() );
+        comment.productId( dto.getProductId() );
+
+        return comment.build();
+    }
+
+    @Override
+    public void toUpdateCommentEntity(CommentUpdateDTO dto, Comment comment) {
+        if ( dto == null ) {
+            return;
+        }
+
+        if ( dto.getRate() != null ) {
+            comment.setRate( dto.getRate() );
+        }
+        if ( dto.getText() != null ) {
+            comment.setText( dto.getText() );
+        }
+        if ( dto.getProductId() != null ) {
+            comment.setProductId( dto.getProductId() );
+        }
+    }
+}
