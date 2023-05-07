bd0741af2c6e9ff14283ee22cf0a9523868521bf
javohir <javokhir544@gmail.com>
2023-05-07 07:45:14 +0500

    COMMIT 9

--- target/generated-sources/annotations/uz/market/uzum/mappers/product/OrderMapperImpl.java
+++ target/generated-sources/annotations/uz/market/uzum/mappers/product/OrderMapperImpl.java
@@ -10,7 +10,7 @@ import uz.market.uzum.enums.Payment;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-06T10:17:37+0500",
+    date = "2023-05-07T07:43:20+0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Private Build)"
 )
 @Component
